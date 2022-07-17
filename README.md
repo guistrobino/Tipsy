5564a7c4b150860aa72496b683486effad06ff33
Guilherme Strobino <guilhermestrobino@Guilhermes-MacBook-Pro.local>
2022-07-17 19:09:49 -0300

    Aplicação finalizada

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tipsy
+
+## Descrição do projeto
+
+Projeto de estudo do curso _iOS & Swift - The Complete iOS App Development Bootcamp_ com a finalidade de criar um aplicativo nativo iOS de divisão de conta com porcentagem de gorjeta.
+
+## Conhecimentos reforçados
+
+- Aplicativos com mais de uma tela com segues
+- Utilização de stepper
+
+## Aplicação
+
+<div style="display: flex; justify-content: space-evenly">
+<img src="./Documentation/Application1.png" width="200px" height="400px">
+<img src="./Documentation/Application2.png" width="200px" height="400px">
+</div>
---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip: Double = 0.0
+    var numberOfPeople = 2
+    var totalForEach = "0.0"
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let buttonTitle = String(sender.currentTitle!).dropLast()
+        
+        tip = Double(buttonTitle)! / 100
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
+        numberOfPeople = Int(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+        let billTotal = Double(billTextField.text!) ?? 0.0
+        
+        let result = (billTotal * (1 + tip)) / Double(numberOfPeople)
+        
+        totalForEach = String(format: "%.2f", result)
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.totalForEach = totalForEach
+            destinationVC.numberOfPeople = String(numberOfPeople)
+            destinationVC.tip = String(format: "%.0f", tip * 100)
+        }
+    }
+    
+
+
+}
+
